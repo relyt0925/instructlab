a18f9e5a9d40d6bed6a572c7d033c5c30816a36b
Tyler Lisowski <lisowski@us.ibm.com>
2025-08-06 20:20:47 -0500

    feat: calculate and log cumulative estimated SDG samples and tokens
    Introduce calculations for cumulative estimated SDG samples and corresponding token counts in taxonomy validation. Enables users to get a quick ballpark estimation of the size  of sdg content based on the estimation techniques at: https://github.com/instructlab/sdg/blob/main/docs/FAQ.md
    
    Signed-off-by: Tyler Lisowski <lisowski@us.ibm.com>

--- src/instructlab/defaults.py
+++ src/instructlab/defaults.py
@@ -110,6 +110,7 @@ class _InstructlabDefaults:
     SDG_SCALE_FACTOR = 30
     SDG_MAX_NUM_TOKENS = 4096
     OPENAI_CLIENT_MAX_RETRIES = 2
+    ESTIMATED_TOKENS_PER_SDG_SAMPLE = 1500
 
     # When otherwise unknown, ilab uses this as the default family
     MODEL_FAMILY = "granite"
